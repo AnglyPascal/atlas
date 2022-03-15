1fe678f755aff49c79031d6e72ad2de6f11b26c8
AnglyPascal <ahsanalmahir@gmail.com>
2022-03-15 16:34:18 +0000

    python stuff

--- Transferable.py
+++ Transferable.py
@@ -1,9 +1,11 @@
 class Transferable:
     def __init__(self):
         self.transfers = []
+        self.num_of_transfers = 0
     def getTransfers(self):
         return self.transfers
     def addTransfer(self, transfer):
         self.transfers.append(transfer)
+        self.num_of_transfers += 1
     def sort(self):
         self.transfers.sort()
