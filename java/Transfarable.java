826d37a411f7355388da6d723bf02bbb490ce758
AnglyPascal <ahsanalmahir@gmail.com>
2022-03-18 20:09:54 +0000

    removed java folder

--- java/Transfarable.java
+++
@@ -1,9 +0,0 @@
-import java.util.TreeSet;
-import java.util.Iterator;
-
-public abstract class Transfarable{
-  public TreeSet<Transfer> transfers;
-  public void addTransfer(Transfer transfer){ transfers.add(transfer); }
-  public Iterator<Transfer> getTransfers(){ return transfers.iterator(); }
-}
-
